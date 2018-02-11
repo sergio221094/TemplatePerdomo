ab6c131370de7ae55468e507f8335e791059de90
sergio221094 <sergio221094@gmail.com>
2018-02-11 01:59:10 -0500

    Puntos de atención y formulario de contacto.

---
+++ enviar2.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Su teléfono es: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'escuelasartesintegradas@gmail.com';
+$asunto = 'Nuevo mensaje de nuestro sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
